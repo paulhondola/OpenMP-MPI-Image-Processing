745c702c68cf0436b05fe08840fa40a24e9aa479
Paul Hondola <paulhondola@gmail.com>
2025-12-05 21:50:35 +0200

    Add common image processing kernels
    Introduces several standard convolution kernels for image processing, including ridge, edge, sharpen, blur, Gaussian (3x3 and 5x5), and unsharp mask (5x5). Header file provides external declarations for use in other modules.

---
+++ src/kernel/kernel.h
@@ -0,0 +1,12 @@
+#ifndef __KERNEL_H__
+#define __KERNEL_H__
+
+extern const double RIDGE_KERNEL[3][3];
+extern const double EDGE_KERNEL[3][3];
+extern const double SHARPEN_KERNEL[3][3];
+extern const double BLUR_KERNEL[3][3];
+extern const double GAUSSIAN3_KERNEL[3][3];
+extern const double GAUSSIAN5_KERNEL[5][5];
+extern const double UNSHARP5_KERNEL[5][5];
+
+#endif
