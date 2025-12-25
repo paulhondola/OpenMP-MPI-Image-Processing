59ec9bdac1df566feb7b630902e215add228bcca
Paul Hondola <paulhondola@gmail.com>
2025-12-25 19:38:32 +0200

    Refactor benchmark result logging into helper functions
    Extracted CSV file initialization and kernel result logging into separate functions (init_csv_files and log_kernel_results) to reduce code duplication and improve readability in write_benchmark_results.

--- src/benchmark/benchmark_io.c
+++ src/benchmark/benchmark_io.c
@@ -7,13 +7,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-app_error write_benchmark_results(int comm_size, BenchmarkConfig config) {
-  // Determine if we are running ALL benchmarks
-  bool run_all = config.run_serial && config.run_multithreaded &&
-                 config.run_distributed && config.run_shared &&
-                 config.run_task_pool;
-
-  // Initialize necessary CSV files
+app_error init_csv_files(BenchmarkConfig config, bool run_all) {
   app_error err = SUCCESS;
   if (run_all) {
     err = init_benchmark_csv(MULTI_RUN_CSV_FILE, MULTI_RUN_CSV_HEADER);
@@ -29,7 +23,90 @@ app_error write_benchmark_results(int comm_size, BenchmarkConfig config) {
     if (!err && config.run_task_pool)
       err = init_benchmark_csv(TASK_POOL_CSV_FILE, SINGLE_RUN_CSV_HEADER);
   }
+  return err;
+}
 
+app_error log_kernel_results(int file_number, int kernel_number, int comm_size,
+                             BenchmarkConfig config, bool run_all, int width,
+                             int height) {
+  app_error err = SUCCESS;
+  double serial_time = -1;
+  if (config.run_serial)
+    serial_time = benchmark_data[0][file_number][kernel_number];
+
+  double multithreaded_time = -1;
+  if (config.run_multithreaded)
+    multithreaded_time = benchmark_data[1][file_number][kernel_number];
+
+  double distributed_time = -1;
+  if (config.run_distributed)
+    distributed_time = benchmark_data[2][file_number][kernel_number];
+
+  double shared_time = -1;
+  if (config.run_shared)
+    shared_time = benchmark_data[3][file_number][kernel_number];
+
+  double task_pool_time = -1;
+  if (config.run_task_pool)
+    task_pool_time = benchmark_data[4][file_number][kernel_number];
+
+  if (run_all) {
+    err = append_benchmark_result(
+        MULTI_RUN_CSV_FILE, width * height, CONV_KERNELS[kernel_number].size,
+        comm_size, config.omp_threads, serial_time, multithreaded_time,
+        distributed_time, shared_time, task_pool_time);
+    if (err != SUCCESS)
+      return err;
+  } else {
+    if (config.run_serial) {
+      err = append_single_benchmark_result(
+          SERIAL_CSV_FILE, width * height, CONV_KERNELS[kernel_number].size,
+          comm_size, config.omp_threads, serial_time);
+      if (err != SUCCESS)
+        return err;
+    }
+    if (config.run_multithreaded) {
+      err = append_single_benchmark_result(
+          MULTITHREADED_CSV_FILE, width * height,
+          CONV_KERNELS[kernel_number].size, comm_size, config.omp_threads,
+          multithreaded_time);
+      if (err != SUCCESS)
+        return err;
+    }
+    if (config.run_distributed) {
+      err = append_single_benchmark_result(DISTRIBUTED_CSV_FILE, width * height,
+                                           CONV_KERNELS[kernel_number].size,
+                                           comm_size, config.omp_threads,
+                                           distributed_time);
+      if (err != SUCCESS)
+        return err;
+    }
+    if (config.run_shared) {
+      err = append_single_benchmark_result(
+          SHARED_CSV_FILE, width * height, CONV_KERNELS[kernel_number].size,
+          comm_size, config.omp_threads, shared_time);
+      if (err != SUCCESS)
+        return err;
+    }
+    if (config.run_task_pool) {
+      err = append_single_benchmark_result(
+          TASK_POOL_CSV_FILE, width * height, CONV_KERNELS[kernel_number].size,
+          comm_size, config.omp_threads, task_pool_time);
+      if (err != SUCCESS)
+        return err;
+    }
+  }
+  return SUCCESS;
+}
+
+app_error write_benchmark_results(int comm_size, BenchmarkConfig config) {
+  // Determine if we are running ALL benchmarks
+  bool run_all = config.run_serial && config.run_multithreaded &&
+                 config.run_distributed && config.run_shared &&
+                 config.run_task_pool;
+
+  // Initialize necessary CSV files
+  app_error err = init_csv_files(config, run_all);
   if (err != SUCCESS) {
     fprintf(stderr, "Failed to initialize CSV file(s): %s\n",
             get_error_string(err));
@@ -47,80 +124,15 @@ app_error write_benchmark_results(int comm_size, BenchmarkConfig config) {
       int height = img->height;
 
       for (int k = 0; k < KERNEL_TYPES; k++) {
-        double serial_time = -1;
-        if (config.run_serial)
-          serial_time = benchmark_data[0][f][k];
-
-        double multithreaded_time = -1;
-        if (config.run_multithreaded)
-          multithreaded_time = benchmark_data[1][f][k];
-
-        double distributed_time = -1;
-        if (config.run_distributed)
-          distributed_time = benchmark_data[2][f][k];
-
-        double shared_time = -1;
-        if (config.run_shared)
-          shared_time = benchmark_data[3][f][k];
-
-        double task_pool_time = -1;
-        if (config.run_task_pool)
-          task_pool_time = benchmark_data[4][f][k];
-
-// Prepare arguments for append
-// Helper block to avoid repetition
-#define APPEND_RESULT(file)                                                    \
-  err = append_benchmark_result(file, width * height, CONV_KERNELS[k].size,    \
-                                comm_size, config.omp_threads, serial_time,    \
-                                multithreaded_time, distributed_time,          \
-                                shared_time, task_pool_time);                  \
-  if (err != SUCCESS) {                                                        \
-    fprintf(stderr, "Failed to append result to %s: %s\n", file,               \
-            get_error_string(err));                                            \
-    return err;                                                                \
-  }
-
-        if (run_all) {
-          APPEND_RESULT(MULTI_RUN_CSV_FILE);
-        } else {
-          if (config.run_serial) {
-            err = append_single_benchmark_result(
-                SERIAL_CSV_FILE, width * height, CONV_KERNELS[k].size,
-                comm_size, config.omp_threads, serial_time);
-            if (err != SUCCESS)
-              return err;
-          }
-          if (config.run_multithreaded) {
-            err = append_single_benchmark_result(
-                MULTITHREADED_CSV_FILE, width * height, CONV_KERNELS[k].size,
-                comm_size, config.omp_threads, multithreaded_time);
-            if (err != SUCCESS)
-              return err;
-          }
-          if (config.run_distributed) {
-            err = append_single_benchmark_result(
-                DISTRIBUTED_CSV_FILE, width * height, CONV_KERNELS[k].size,
-                comm_size, config.omp_threads, distributed_time);
-            if (err != SUCCESS)
-              return err;
-          }
-          if (config.run_shared) {
-            err = append_single_benchmark_result(
-                SHARED_CSV_FILE, width * height, CONV_KERNELS[k].size,
-                comm_size, config.omp_threads, shared_time);
-            if (err != SUCCESS)
-              return err;
-          }
-          if (config.run_task_pool) {
-            err = append_single_benchmark_result(
-                TASK_POOL_CSV_FILE, width * height, CONV_KERNELS[k].size,
-                comm_size, config.omp_threads, task_pool_time);
-            if (err != SUCCESS)
-              return err;
-          }
+        err =
+            log_kernel_results(f, k, comm_size, config, run_all, width, height);
+        if (err != SUCCESS) {
+          fprintf(stderr,
+                  "Failed to append result for file %s, kernel %d: %s\n",
+                  files[f], k, get_error_string(err));
+          free_BMP(img);
+          return err;
         }
-
-#undef APPEND_RESULT
       }
       free_BMP(img);
     } else {
